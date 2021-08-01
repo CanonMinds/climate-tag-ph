45f243f138610efba2d9bd48c1de6a66dcf12a24
Ray <balafranzraymond@gmail.com>
2021-08-02 00:59:28 +0800

    fix: updated InitialModels fields

--- climate_app/admin.py
+++ climate_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from climate_app.models import InitialModel
+
+class InitialModelAdmin(admin.ModelAdmin):
+    list_display = ('sovereignty', )
+    search_fields = ('sovereignty', )
+
+admin.site.register(InitialModel, InitialModelAdmin)
--- climate_app/models.py
+++ climate_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class InitialModel(models.Model):
+    sovereignty = models.CharField(max_length=100, null=False)
+    geometry = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Country'
+        verbose_name_plural = 'Countries'

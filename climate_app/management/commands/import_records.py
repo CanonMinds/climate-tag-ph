db02b1f32b8203de8e4f5b47c3cfcc6ea9e6a84b
Ray <balafranzraymond@gmail.com>
2021-08-08 22:41:47 +0800

    fix: updated import records ordering

--- climate_app/management/commands/import_records.py
+++ climate_app/management/commands/import_records.py
@@ -10,12 +10,13 @@ from pprint import pprint
 from climate_app.models import DACMemberCountry
 from climate_app.models import InitialModel
 
+
 class Command(BaseCommand):
     def handle(self, **options):
-        encoding="utf-8"
-        file_path = 'C:/Django/ICSC/climate-tagging-ph/fixtures/dac_sample.csv'
+        encoding = "utf-8"
+        file_path = "C:/Django/ICSC/climate-tagging-ph/fixtures/dac_sample.csv"
         with open(file_path, "rt", encoding=encoding) as csv_file_input:
-            csv_reader =  csv.reader(csv_file_input, delimiter=',')
+            csv_reader = csv.reader(csv_file_input, delimiter=",")
             line_count = 0
 
             DACMemberCountry.objects.all().delete()
@@ -26,36 +27,38 @@ class Command(BaseCommand):
                     line_count += 1
 
                 else:
-                    pprint ("")
-                    pprint ("ROW NUMBER: %s" % line_count)
-                    
-                    if row[0] != "":
-                        country_code=row[0]
-                        year=row[1]
-                        provider_type=row[2]
-                        provider=row[3]
-                        provider_code=row[4]
-                        adaptation_objective=row[5]
-                        mitigation_objective=row[6]
-                        adaptation_dev_finance_commitment_2019=row[7]
-                        adaptation_dev_finance_commitment_current=row[8]
-                        mitigation_dev_finance_commitment_2019=row[9]
-                        mitigation_dev_finance_commitment_current=row[10]
-                        overlap_commitment_2019=row[11]
-                        overlap_commitment_current=row[12]
-                        climate_dev_finance_commitment_2019=row[13]
-                        climate_dev_finance_commitment_current=row[14]
-                        channel_delivery_code=row[15]
-                        channel_delivery=row[16]
-                        purpose_code=row[17]
-                        sector=row[18]
-                        sub_sector=row[19]
-                        development_cooperation_modality=row[20]
-                        financial_instrument=row[21]
-                        finance_type=row[22]
-                        gender=row[23]
+                    pprint("")
+                    pprint("ROW NUMBER: %s" % line_count)
 
-                        country_code_value = InitialModel.objects.get(country_code=int(country_code))
+                    if row[0] != "":
+                        country_code = row[0]
+                        year = row[1]
+                        provider_type = row[2]
+                        provider = row[3]
+                        provider_code = row[4]
+                        adaptation_objective = row[5]
+                        mitigation_objective = row[6]
+                        adaptation_dev_finance_commitment_2019 = row[8]
+                        adaptation_dev_finance_commitment_current = row[7]
+                        mitigation_dev_finance_commitment_2019 = row[10]
+                        mitigation_dev_finance_commitment_current = row[9]
+                        overlap_commitment_2019 = row[12]
+                        overlap_commitment_current = row[11]
+                        climate_dev_finance_commitment_2019 = row[14]
+                        climate_dev_finance_commitment_current = row[13]
+                        channel_delivery_code = row[15]
+                        channel_delivery = row[16]
+                        purpose_code = row[17]
+                        sector = row[18]
+                        sub_sector = row[19]
+                        development_cooperation_modality = row[20]
+                        financial_instrument = row[21]
+                        finance_type = row[22]
+                        gender = row[23]
+
+                        country_code_value = InitialModel.objects.get(
+                            country_code=int(country_code)
+                        )
                         record = DACMemberCountry(
                             country_code=country_code_value,
                             year=year,
@@ -84,10 +87,13 @@ class Command(BaseCommand):
                         )
                         record.save()
                         line_count += 1
-                        pprint (provider + " CREATED!")
-                        pprint ("#" * 70)
+                        pprint(provider + " CREATED!")
+                        pprint("#" * 70)
                     else:
                         return False
 
-                logging.info("PROCESSED -'%s' LINES. OECD DATA IMPORT SUCCESS." % (int(line_count)-1))
+                logging.info(
+                    "PROCESSED -'%s' LINES. OECD DATA IMPORT SUCCESS."
+                    % (int(line_count) - 1)
+                )
         csv_file_input.close()

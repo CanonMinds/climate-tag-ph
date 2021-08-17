d6a7171ea3c8879c5866103f250cf6c01cbdeb26
Ray <balafranzraymond@gmail.com>
2021-08-18 01:55:07 +0800

    feature: Introduced major UI changes

--- climate_app/urls.py
+++ climate_app/urls.py
@@ -4,15 +4,15 @@ from . import views
 app_name = "climate_app"
 
 urlpatterns = [
-    url(r"source-map/$", views.dashboard, name="source_map_dash"),
+    url(r"source-map/$", views.SourceMapView.as_view(), name="source_map_dash"),
     url(
         r"climate-objective/$",
-        views.dashboard,
+        views.ClimateObjectiveView.as_view(),
         name="climate_objective_dash",
     ),
     url(
         r"sectoral-funding/$",
-        views.dashboard,
+        views.SectoralFundingView.as_view(),
         name="sectoral_funding_dash",
     ),
 ]
--- climate_app/views.py
+++ climate_app/views.py
@@ -14,77 +14,79 @@ class SourceMapView(ListView):
     template_name = "climate_app/dash_source_map.html"
 
     def get_context_data(self, **kwargs):
-
-        """FeatureCollection - Coordinate Serialization"""
-        coordinate_queries = (
-            DACMemberCountry.objects.filter(
-                country_code__country_code__gt=101,
-                country_code__country_code__lte=108,
+        process=None
+        if process:
+            """FeatureCollection - Coordinate Serialization"""
+            coordinate_queries = (
+                DACMemberCountry.objects.filter(
+                    country_code__country_code__gt=101,
+                    country_code__country_code__lte=108,
+                )
+                .exclude(country_code__country_code__in=[105, 106])
+                .values(
+                    "id",
+                    "provider",
+                    "country_code__geometry",
+                    "provider",
+                    "year",
+                    "climate_dev_finance_commitment_current",
+                    "financial_instrument",
+                    "finance_type",
+                )
             )
-            .exclude(country_code__country_code__in=[105, 106])
-            .values(
-                "id",
-                "provider",
-                "country_code__geometry",
-                "provider",
-                "year",
-                "climate_dev_finance_commitment_current",
-                "financial_instrument",
-                "finance_type",
-            )
-        )
 
-        geojson_format = {
-            "type": "FeatureCollection",
-            "name": "DATA",
-            "crs": {
-                "type": "name",
-                "properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84"},
-            },
-        }
+            geojson_format = {
+                "type": "FeatureCollection",
+                "name": "DATA",
+                "crs": {
+                    "type": "name",
+                    "properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84"},
+                },
+            }
 
-        features_list = []
-        if coordinate_queries:
-            for record in coordinate_queries:
+            features_list = []
+            if coordinate_queries:
+                for record in coordinate_queries:
 
-                feature_format = {
-                    "type": "Feature",
-                    "properties": "",
-                    "geometry": "",
-                }
+                    feature_format = {
+                        "type": "Feature",
+                        "properties": "",
+                        "geometry": "",
+                    }
 
-                property_serializer = {}
-                property_serializer["Provider"] = record["provider"]
-                property_serializer["Year"] = record["year"]
-                if float(record["climate_dev_finance_commitment_current"]):
-                    property_serializer[
-                        "ClimateDevFinanceCommitmentCurrent"
-                    ] = "$" + str(
-                        float(record["climate_dev_finance_commitment_current"])
-                    )
-                property_serializer["FinancialInstrument"] = record[
-                    "financial_instrument"
-                ]
-                property_serializer["FinanceType"] = record["finance_type"]
-                property_serializer["Attribute"] = ""
+                    property_serializer = {}
+                    property_serializer["Provider"] = record["provider"]
+                    property_serializer["Year"] = record["year"]
+                    if float(record["climate_dev_finance_commitment_current"]):
+                        property_serializer[
+                            "ClimateDevFinanceCommitmentCurrent"
+                        ] = "$" + str(
+                            float(record["climate_dev_finance_commitment_current"])
+                        )
+                    property_serializer["FinancialInstrument"] = record[
+                        "financial_instrument"
+                    ]
+                    property_serializer["FinanceType"] = record["finance_type"]
+                    property_serializer["Attribute"] = ""
 
-                geometry_serializer = {}
-                geometry_serializer["type"] = "MultiPolygon"
+                    geometry_serializer = {}
+                    geometry_serializer["type"] = "MultiPolygon"
 
-                jsonDec = json.decoder.JSONDecoder()
-                myPythonList = jsonDec.decode(record["country_code__geometry"])
+                    jsonDec = json.decoder.JSONDecoder()
+                    myPythonList = jsonDec.decode(record["country_code__geometry"])
 
-                geometry_serializer["coordinates"] = myPythonList
-                geometry_serializer["id"] = record["id"]
+                    geometry_serializer["coordinates"] = myPythonList
+                    geometry_serializer["id"] = record["id"]
 
-                feature_format["properties"] = property_serializer
-                feature_format["geometry"] = geometry_serializer
+                    feature_format["properties"] = property_serializer
+                    feature_format["geometry"] = geometry_serializer
 
-                features_list.append(feature_format)
+                    features_list.append(feature_format)
 
-        geojson_format["features"] = features_list
-        coordinate_query = json.dumps(geojson_format)
-
+            geojson_format["features"] = features_list
+            coordinate_query = json.dumps(geojson_format)
+        else:
+            coordinate_query = None
         context = super(ListView, self).get_context_data(**kwargs)
         context = {
             "coordinate_query": coordinate_query,
@@ -94,7 +96,7 @@ class SourceMapView(ListView):
 
 class ClimateObjectiveView(ListView):
     model = DACMemberCountry
-    template_name = "climate_app/dash_climate_objective.html"
+    template_name = "climate_app/charts.html"
 
     def get_context_data(self, **kwargs):
         coordinate_queries = (
@@ -153,7 +155,7 @@ class SectoralFundingView(ListView):
 
 
 def index(request):
-    template_name = "climate_app/dash_base.html"
+    template_name = "climate_app/dash_index.html"
     return render(
         request,
         template_name,
